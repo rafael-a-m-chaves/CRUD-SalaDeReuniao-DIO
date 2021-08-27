79a3d3f87383cedef1a0290682ceb40c644de602
rafael-a-m-chaves <rafaelaugustochaves@hotmail.com>
2021-08-27 11:49:51 -0300

    Create Class Controller for Model Room

--- src/main/java/com/digital/crud/saladereuniao/saladereuniao/controller/RoomController.java
+++ src/main/java/com/digital/crud/saladereuniao/saladereuniao/controller/RoomController.java
@@ -1,2 +1,9 @@
-package com.digital.crud.saladereuniao.saladereuniao.controller;public class RoomController {
+package com.digital.crud.saladereuniao.saladereuniao.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RoomController {
+
+
 }
