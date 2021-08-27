2ee1a2af89e75e51b7d3e02d321da1b997f464ec
rafael-a-m-chaves <rafaelaugustochaves@hotmail.com>
2021-08-27 11:51:25 -0300

    Create and Implements Class errors

--- src/main/java/com/digital/crud/saladereuniao/saladereuniao/exception/ErrorDetails.java
+++ src/main/java/com/digital/crud/saladereuniao/saladereuniao/exception/ErrorDetails.java
@@ -1,2 +1,19 @@
-package com.digital.crud.saladereuniao.saladereuniao.exception;public class ErrorDetails {
+package com.digital.crud.saladereuniao.saladereuniao.exception;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ErrorDetails {
+
+    private Date timestemp;
+    private String message;
+    private String details;
+
+
 }
--- src/main/java/com/digital/crud/saladereuniao/saladereuniao/exception/ResourceNotFund.java
+++ src/main/java/com/digital/crud/saladereuniao/saladereuniao/exception/ResourceNotFund.java
@@ -1,2 +1,16 @@
-package com.digital.crud.saladereuniao.saladereuniao.exception;public class ResourceNotFund {
+package com.digital.crud.saladereuniao.saladereuniao.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFund extends Exception{
+
+    private static final Long serialVersionUID= 1l;
+
+    public ResourceNotFund(String message){
+        super(message);
+    }
+
 }
